bc91c89de3e3466eb81740282b0573f208d81f2a
Ebert Mahon <ebertmahon@gmail.com>
2017-09-20 02:50:43 +0000

    updated status to show blank and draw boards

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,20 +15,28 @@ WIN_COMBINATIONS = [
   [2,4,6] # Diagonal right to left
 ]
 
-def won?(board, index)
-  WIN_COMBINATIONS.do each |win_combination|
+def won?(board)
+  if board == [" "," "," "," "," "," "," "," "," "] || nil
+    return false  # board is empty
+  end
+
+  if board == ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+    return false  # game is a draw
+  end
+
+  WIN_COMBINATIONS.each do |win_combination|
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
   win_index_3 = win_combination[2]
 
-  position_1 = board(win_index_1)
-  position_2 = board(win_index_2)
-  position_3 = board(win_index_3)
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
 
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination
-  else
-    false
-  end
-end
-end
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combination
+      else
+        false
+      end
+    end
+  end  
